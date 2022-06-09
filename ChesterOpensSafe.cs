69144c28c71d5508acdf866c0af4e1e482d1d5c3
Sheepish Shepherd <cameronscottmcdougal@gmail.com>
2022-06-08 22:38:54 -0400

    Fixed sounds playing repeatedly
    +Added comments
    +Simplified coding method

--- ChesterOpensSafe.cs
+++ ChesterOpensSafe.cs
@@ -11,64 +11,55 @@ namespace ChesterOpensSafe
 
 	public class ChesterPlayer : ModPlayer
 	{
+		// A bool to flag if Chester is open.
+		// //This will not be reset each update as we want it to carry over into the next tick.
 		bool handlingChester = false;
-		int LocalChester = -1;
 
-		public override void PostUpdateMiscEffects() {
-			if (Player.piggyBankProjTracker.ProjectileType == ProjectileID.ChesterPet || handlingChester) {
-				HandleChesterSafe();
+		public override void PreUpdate() {
+			// Temporarily set the player's chest to Piggy Bank before the vanilla Chest Handling code is run.
+			// Chest Handling for Chester's inventory will close if it is not set to -2.
+			if (handlingChester) {
+				Player.chest = -2;
+			}
+		}
+
+		public override void PostUpdate() {
+			// After vanilla Chest Handling code is run, determine if it actually was Chester.
+			// If so, run the modded ChesterHandling code.
+			handlingChester = Player.piggyBankProjTracker.ProjectileType == ProjectileID.ChesterPet;
+			if (handlingChester) {
+				HandleChesterSafe(Main.projectile[Player.piggyBankProjTracker.ProjectileLocalIndex]);
 			}
 		}
 
 		private void UnhandleChester(string debugMessage) {
+			// To prevent anything breaking, clear the piggyBankTracker and reset the bool.
 			Player.piggyBankProjTracker.Clear();
 			handlingChester = false;
-			LocalChester = -1;
-
-			// NewText for debugging
-			//Main.NewText(debugMessage);
+			Recipe.FindRecipes();
+			//Main.NewText(debugMessage); // NewText for debugging
 		}
 
-		private void HandleChesterSafe() {
-			if (Player.chest == -2) {
-				LocalChester = Player.piggyBankProjTracker.ProjectileLocalIndex;
-
-				handlingChester = true;
-				Player.chest = -3;
-				Recipe.FindRecipes();
-			}
-			else if (handlingChester) {
-				if (LocalChester >= 0) {
-					Projectile projectile = Main.projectile[LocalChester];
-					if (!projectile.active || projectile.type != ProjectileID.ChesterPet) {
-						UnhandleChester("Projectile is inactive or not the correct type");
-						return;
-					}
-					else {
-						Player.piggyBankProjTracker.Set(projectile);
-						LocalChester = Player.piggyBankProjTracker.ProjectileLocalIndex;
-						handlingChester = true;
-						Player.chest = -3;
-
-						int x = (int)(((double)Player.position.X + (double)Player.width * 0.5) / 16.0);
-						int y = (int)(((double)Player.position.Y + (double)Player.height * 0.5) / 16.0);
-						bool xOutOfRange = x < Player.chestX - Player.tileRangeX || x > Player.chestX + Player.tileRangeX + 1;
-						bool yOutOfRange = y < Player.chestY - Player.tileRangeY || y > Player.chestY + Player.tileRangeY + 1;
-						if (xOutOfRange || yOutOfRange) {
-							UnhandleChester("Projectile is too far from the player");
-							return;
-						}
-					}
-					Player.piggyBankProjTracker.Set(projectile);
-					Recipe.FindRecipes();
-				}
-				else {
-					UnhandleChester("Projectile index is -1");
-				}
+		private void HandleChesterSafe(Projectile chester) {
+			// Run through normal vanilla chest handling for Chester.
+			if (!chester.active || chester.type != ProjectileID.ChesterPet) {
+				UnhandleChester("Projectile is inactive or not the correct type");
+				return;
 			}
 			else {
-				UnhandleChester("Player bank is not -2 nor -3");
+				int x = (int)(((double)Player.position.X + (double)Player.width * 0.5) / 16.0);
+				int y = (int)(((double)Player.position.Y + (double)Player.height * 0.5) / 16.0);
+				bool xOutOfRange = x < Player.chestX - Player.tileRangeX || x > Player.chestX + Player.tileRangeX + 1;
+				bool yOutOfRange = y < Player.chestY - Player.tileRangeY || y > Player.chestY + Player.tileRangeY + 1;
+				if (xOutOfRange || yOutOfRange) {
+					UnhandleChester("Projectile is too far from the player");
+					return;
+				}
 			}
+			// Will only get to this point if projectile is active, is the correct type, and is within range.
+			// Set the Player's chest inventory to the Safe's inventory.
+			Player.chest = -3;
+			Recipe.FindRecipes();
 		}
 	}
 }
