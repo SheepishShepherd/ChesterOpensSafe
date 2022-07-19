8293bc57ed7d095a2008d27704835b922fd08a55
Sheepish Shepherd <cameronscottmcdougal@gmail.com>
2022-07-19 01:30:53 -0400

    More handling cases
    • Handling checks for Safe, Piggybank, and Money Trough interactions for cleaner interactions
    • Auto-Pause now supported
    • Chest UI now closes properly when interacting with Chester
    • Chest UI now closes properly when closing the player inventory

--- ChesterOpensSafe.cs
+++ ChesterOpensSafe.cs
@@ -1,3 +1,4 @@
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -6,26 +7,76 @@ namespace ChesterOpensSafe
 {
 	public class ChesterOpensSafe : Mod
 	{
-
+		// TODO: [Issue] Chest remains at -2 for a single frame
 	}
 
 	public class ChesterPlayer : ModPlayer
 	{
 		// A bool to flag if Chester is open.
-		// //This will not be reset each update as we want it to carry over into the next tick.
+		// This will not be reset each update as we want it to carry over into the next tick.
 		bool handlingChester = false;
 
+		// Temporarily set the player's chest to Piggy Bank before the vanilla Chest Handling code is run.
+		// Chest Handling for Chester's inventory will close if it is not set to -2.
 		public override void PreUpdate() {
-			// Temporarily set the player's chest to Piggy Bank before the vanilla Chest Handling code is run.
-			// Chest Handling for Chester's inventory will close if it is not set to -2.
 			if (handlingChester) {
 				Player.chest = -2;
+
+				// Prevents Chester being considered 'open' when accessing a Piggy Bank or Safe
+				if (Main.mouseRight && Main.mouseRightRelease && Player.BlockInteractionWithProjectiles == 0) {
+					if (Player.IsTileTypeInInteractionRange(TileID.Safes) && Main.tile[Player.tileTargetX, Player.tileTargetY].TileType == TileID.Safes) {
+						UnhandleChester("A Safe was opened via right-click");
+						return;
+					}
+					// Smart Interact can be a non-tile, so check if its within the bounds
+					if (Main.SmartInteractX >= 0 && Main.SmartInteractX <= Main.maxTilesX && Main.SmartInteractY >= 0 && Main.SmartInteractY <= Main.maxTilesY) {
+						if (Main.tile[Main.SmartInteractX, Main.SmartInteractY].TileType == TileID.Safes) {
+							UnhandleChester("A Safe was opened via smart-cursor");
+							return;
+						}
+					}
+
+					if (Player.IsTileTypeInInteractionRange(TileID.PiggyBank) && Main.tile[Player.tileTargetX, Player.tileTargetY].TileType == TileID.PiggyBank) {
+						UnhandleChester("A Piggy Bank was opened via right-click");
+						return;
+					}
+					if (Main.SmartInteractX >= 0 && Main.SmartInteractX <= Main.maxTilesX && Main.SmartInteractY >= 0 && Main.SmartInteractY <= Main.maxTilesY) {
+						if (Main.tile[Main.SmartInteractX, Main.SmartInteractY].TileType == TileID.PiggyBank) {
+							UnhandleChester("A Piggy Bank was opened via smart-cursor");
+							return;
+						}
+					}
+				}
 			}
 		}
 
+		// Until the first frame issue is resolved, this will allow users with autopause to access the safe when using Chester
+		public override void UpdateAutopause() {
+			if (Player.piggyBankProjTracker.ProjectileType == ProjectileID.ChesterPet) {
+				Player.chest = -3;
+				Recipe.FindRecipes();
+			}
+		}
+
+		// After vanilla Chest Handling code is run, determine if it Chester was being used.
+		// If so, run the modded Chester handling code.
 		public override void PostUpdate() {
-			// After vanilla Chest Handling code is run, determine if it actually was Chester.
-			// If so, run the modded ChesterHandling code.
+			// Prevents Chester from closing when accessing it while already being in a piggy bank chest
+			if (!handlingChester && Player.chest == -2 && Main.mouseRight && Main.mouseRightRelease && Player.BlockInteractionWithProjectiles == 0) {
+				int index = Player.GetListOfProjectilesToInteractWithHack().FindIndex(x => Main.projectile[x].type == ProjectileID.ChesterPet);
+				if (index != -1 && Main.projectile[index].Hitbox.Contains(Main.MouseWorld.ToPoint())) {
+					Player.chest = -3;
+					Recipe.FindRecipes();
+					return;
+				}
+				else if (Main.SmartInteractProj != -1 && Main.projectile[Main.SmartInteractProj].type == ProjectileID.ChesterPet) {
+					Player.chest = -3;
+					Recipe.FindRecipes();
+					return;
+				}
+			}
+
+			// Run the Chester handling code if Chester is being used
 			handlingChester = Player.piggyBankProjTracker.ProjectileType == ProjectileID.ChesterPet;
 			if (handlingChester) {
 				HandleChesterSafe(Main.projectile[Player.piggyBankProjTracker.ProjectileLocalIndex]);
@@ -37,27 +88,40 @@ namespace ChesterOpensSafe
 			Player.piggyBankProjTracker.Clear();
 			handlingChester = false;
 			Recipe.FindRecipes();
-			//Main.NewText(debugMessage); // NewText for debugging
+			Main.NewText(debugMessage); // NewText for debugging
 		}
 
 		private void HandleChesterSafe(Projectile chester) {
-			// Run through normal vanilla chest handling for Chester.
+			// Run through each case where Chester should be closed...
+
 			if (!chester.active || chester.type != ProjectileID.ChesterPet) {
 				UnhandleChester("Projectile is inactive or not the correct type");
 				return;
 			}
-			else {
-				int x = (int)(((double)Player.position.X + (double)Player.width * 0.5) / 16.0);
-				int y = (int)(((double)Player.position.Y + (double)Player.height * 0.5) / 16.0);
-				bool xOutOfRange = x < Player.chestX - Player.tileRangeX || x > Player.chestX + Player.tileRangeX + 1;
-				bool yOutOfRange = y < Player.chestY - Player.tileRangeY || y > Player.chestY + Player.tileRangeY + 1;
-				if (xOutOfRange || yOutOfRange) {
-					UnhandleChester("Projectile is too far from the player");
+
+			if (!Main.playerInventory) {
+				UnhandleChester("Inventory was closed");
+				return;
+			}
+
+			Vector2 compareSpot = Player.Center;
+			if (!Player.IsProjectileInteractibleAndInInteractionRange(chester, ref compareSpot)) {
+				UnhandleChester("Projectile is too far from the player or uninteractible");
+				return;
+			}
+
+			if (Main.mouseRight && Main.mouseRightRelease && Player.BlockInteractionWithProjectiles == 0) {
+				if (chester.Hitbox.Contains(Main.MouseWorld.ToPoint())) {
+					UnhandleChester("Closed Chester via right-click");
+					return;
+				}
+				if (Main.SmartInteractProj == Player.piggyBankProjTracker.ProjectileLocalIndex) {
+					UnhandleChester("Closed Chester via smart-cursor");
 					return;
 				}
 			}
-			// Will only get to this point if projectile is active, is the correct type, and is within range.
-			// Set the Player's chest inventory to the Safe's inventory.
+
+			// Set the Player's chest to the safe inventory.
 			Player.chest = -3;
 			Recipe.FindRecipes();
 		}
